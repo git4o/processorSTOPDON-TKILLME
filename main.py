a9850081a5edc84f6b5819edf257d9086ddc97ff
O <>
2021-06-03 23:19:05 +0000

    stop

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-code = {"code":"its what makes a program a progeam","boolean":"fancy way of saying true of false"}
+code = {"code":"its what makes a program a progeam",
+"boolean":"fancy way of saying true of false",
+"binary":""}
+#I'm the stupedits person alive, i make  fake ransomwares (tey just stop u from opening you file explorer)
