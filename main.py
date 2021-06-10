092605ef298e0d4dbcaf5799bea0df8374bf3466
O <71034923+git4o@users.noreply.github.com>
2021-06-10 23:45:27 +0000

    myhem

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 #tic tac toe
 import secondary
-grid = ["__" for i in range(9) ]
+
+grid = ["__" for i in range(9)]
+
 
 def play():
-	for i in range(0,9,3):
-		print(grid[i]+" "+grid[i+1]+" "+grid[i+2])
+  for i in range(0, 9, 3):
+    print(grid[i] + " " + grid[i + 1] + " " + grid[i + 2])
+
 
 turn = True
 
 while "__" in grid:
 	play()
-	if "__" != grid[0] and grid[0] == grid[4] and grid [4] == grid [8]:
-		print(grid[0] +"wins")
-	answer = int(input("What space"))
+	if ("__" != grid[0] and grid[0] == grid[4] and grid[4] == grid[8]) or ("__" != grid[2] and grid[2] == grid[4] and grid[4] == grid[6]) or ("__" != grid[0] and grid[0] == grid[1] and grid[1] == grid[2]) or ("__" != grid[3] and grid[3] == grid[4] and grid[4] == grid[5]) or ("__" != grid[6] and grid[6] == grid[7]and grid[7] == grid[8]):
+		print(grid[0] + " wins")
+		quit()
+	answer = int(input("What space\n>"))
 	if answer != 0:
-		if turn:
-			grid[answer-1] = "x"
-		else:
-			grid[answer-1] = "o"
+		if grid[answer] == "__":
+			if turn:
+				grid[answer - 1] = "x"
+			else:
+				grid[answer - 1] = "o"
 		turn = not turn
 	else:
 		if turn:
-			grid = ["x"] *9
-		else:
-			grid = ["o"] *9
+			grid = ["x"] * 9
+			play()
+			print("x wins")
+			quit()
+    else:
+      grid = ["o"] * 9
+      play()
+      print("o wins")
+      quit()
 play()
+print("Tie")
--- secondary.py
+++ secondary.py
@@ -1,6 +1,22 @@
 import time
 from colorama import *
 print("//////////////////////////////////////////////////\n")
-print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n")
+print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n")
+print("===================================================\n")
+print(Fore.RED+"The. "+Fore.BLUE+"Ultimate. "+Fore.GREEN+"TIC-TO-Game"+Style.RESET_ALL+"!!!!!!!!\n")
 print("===================================================")
-print(Fore.RED+"The. "+Fore.BLUE+"Ultimate. "+Fore.GREEN+"TIC-TO-Game"+Style.RESET_ALL+"!!!!!!!!")
+print("//////////////////////////////////////////////////\n")
+print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n")
+names = ["You wanna poop in my pants?","Farting...","Like poop?","Funa fact, this was made by a 9 & 10 year old","TIC-TAC-TO IS FARTIBLE","ZOOM ZOOMING","\"TIE\"","DIE!!!!!!!!"]
+print('Made By Ryder & Orion')
+import os
+import random as fart
+
+print('TIP OF THE RUN:')
+print(fart.choice(names))
+print('LOADING...')
+
+
+
+time.sleep(5)
+os.system('clear')
