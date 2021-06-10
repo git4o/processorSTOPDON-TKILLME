d2d03fc792cb91dd1aea9ba2736fa6ae622afb12
O <71034923+git4o@users.noreply.github.com>
2021-06-10 23:17:18 +0000

    tr

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-code = {"code":"its what makes a program a progeam",
-"boolean":"fancy way of saying true of false",
-"binary":""}
-#I'm the stupedits person alive, i make  fake ransomwares (tey just stop u from opening you file explorer)
+#tic tac toe
+
+grid = ["__" for i in range(9) ]
+
+def play():
+	for i in range(0,9,3):
+		print(grid[i]+" "+grid[i+1]+" "+grid[i+2])
+
+turn = True
+
+while "__" in grid:
+	play()
+	answer = int(input("What space"))
+	if answer != 0:
+		if turn:
+			grid[answer-1] = "x"
+		else:
+			grid[answer-1] = "o"
+		turn = not turn
+	else:
+		if turn:
+			grid = ["x"] *9
+		else:
+			grid = ["o"] *9
+play()
